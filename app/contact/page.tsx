8048b6d91f7d84d79df1df5a3f83dbfd96d50a23
Huann Almeida <huannvictor@gmail.com>
2024-04-23 16:26:38 -0300

    update: text updated

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,21 +6,25 @@ const Contact = () => {
   return (
     <section>
       <div className="container mx-auto">
-        <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
+        <div className="grid xl:grid-cols-2 lg:gap-10 pt-12 xl:h-[600px] mb-6 xl:mb-24">
           <div className="flex flex-col justify-center">
             <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
               <span className="w-[30px] h-[2px] bg-primary" />
               Say Hello 👋
             </div>
             <h1 className="h1 max-w-md mb-8">Let&apos;s Work Together.</h1>
-            <p className="subtitle max-w-[400px]">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Reiciendis magni repellendus in vitae veniam at error voluptas
-              vel, suscipit cum libero illo labore neque.
+            <p className="subtitle xl:max-w-2/3">
+              Looking for an experienced front-end developer? Your search ends here!
+              <br />
+              As a freelancer front-end developer and current Tech Lead at SkillLab, a dynamic startup, I bring a wealth of experience in building and leading high-performing development teams.
+              <br />
+              Proficient in React.js, Next.js, and Tailwind CSS, I'm ready to bring my leadership and technical skills to your organization. Moreover, while I excel in these technologies, I'm also open to learning and working with others like Angular and various JavaScript libraries/frameworks.
+              <br />
+              Let's collaborate and drive innovation together.
             </p>
           </div>
 
-          <div className="hidden xl:flex w-full bg-contact_illustrator_light dark:bg-contact_illustrator_dark bg-contain bg-top bg-no-repeat"></div>
+          <div className="hidden xl:flex xl:w-full bg-contact_illustrator_light dark:bg-contact_illustrator_dark bg-contain bg-top bg-no-repeat"></div>
         </div>
 
         <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">

74ed185e7104c8914365bbd893d3cf67f26cec35
Huann Almeida <huannvictor@gmail.com>
2024-05-06 20:35:44 -0300

    lint: sorte page.tsx imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Cta from '@/src/components/Cta'
 import AboutSection from '@/src/pageSections/AboutSection'
 import HeroSection from '@/src/pageSections/HeroSection'
 import ServicesSection from '@/src/pageSections/ServicesSection'
-import WorkSection from '../pageSections/WorkSection'
+import WorkSection from '@/src/pageSections/WorkSection'
 // import Reviews from '@/components/Reviews'
 
 export default function Home() {

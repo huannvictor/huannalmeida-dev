3f989e1487f1bee511116ebd5c98cd0ee0bce263
Huann <huannvictor@gmail.com>
2025-02-05 13:27:16 -0300

    refatora componente WorkCardsCarousel para melhorar a legibilidade e consistência do código, ajustando a formatação e os imports

--- src/components/work/WorkCardsCarousel.tsx
+++ src/components/work/WorkCardsCarousel.tsx
@@ -1,50 +1,49 @@
-"use client"
+"use client";
 
-import 'swiper/css'
-import 'swiper/css/pagination'
+import "swiper/css";
+import "swiper/css/pagination";
 
-import {Pagination} from 'swiper/modules'
-import {Swiper, SwiperSlide} from 'swiper/react'
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import ProjectCard from '../ProjectCard'
+import ProjectCard from "../ProjectCard";
 
 interface WorkCadsCarouselProps {
-  projectData: ProjectData[]
+	projectData: ProjectData[];
 }
 
-export default function WorkCadsCarousel ({
-  projectData
-}: WorkCadsCarouselProps){
-  return (
-    <div className="max-w-[600px] lg:col-span-2">
-      <Swiper
-        className="h-[480px]"
-        slidesPerView={1}
-        breakpoints={{
-          640: {slidesPerView: 2},
-        }}
-        spaceBetween={30}
-        modules={[Pagination]}
-        pagination={{clickable: true}}
-      >
-        {projectData.slice(0, 4).map((project, index) => {
-          const {category, description, github, image, link, name} =
-            project
+export default function WorkCadsCarousel({
+	projectData,
+}: WorkCadsCarouselProps) {
+	return (
+		<div className="max-w-[600px] lg:col-span-2">
+			<Swiper
+				className="h-[480px]"
+				slidesPerView={1}
+				breakpoints={{
+					640: { slidesPerView: 2 },
+				}}
+				spaceBetween={30}
+				modules={[Pagination]}
+				pagination={{ clickable: true }}
+			>
+				{projectData.slice(0, 4).map((project, index) => {
+					const { category, description, github, image, link, name } = project;
 
-          return (
-            <SwiperSlide key={index}>
-              <ProjectCard
-                image={image}
-                category={category}
-                name={name}
-                description={description}
-                link={link}
-                github={github}
-              />
-            </SwiperSlide>
-          )
-        })}
-      </Swiper>
-    </div>
-  );
-};
+					return (
+						<SwiperSlide key={index}>
+							<ProjectCard
+								image={image}
+								category={category}
+								name={name}
+								description={description}
+								link={link}
+								github={github}
+							/>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</div>
+	);
+}

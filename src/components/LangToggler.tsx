47e6f0fad3cb30562635fb93e2347d688588f601
Huann <huannvictor@gmail.com>
2025-02-05 13:26:40 -0300

    refatora componente LangToggler para melhorar a legibilidade e consistência do código, ajustando a formatação e estilos

--- src/components/LangToggler.tsx
+++ src/components/LangToggler.tsx
@@ -1,30 +1,34 @@
-'use client'
+"use client";
 
-import {LanguagesIcon} from 'lucide-react'
-import {useState} from 'react'
+import { LanguagesIcon } from "lucide-react";
+import { useState } from "react";
 
-import {Button} from '../../components/ui/button'
-import {HoverCard, HoverCardContent, HoverCardTrigger} from '../../components/ui/hover-card'
+import { Button } from "../../components/ui/button";
+import {
+	HoverCard,
+	HoverCardContent,
+	HoverCardTrigger,
+} from "../../components/ui/hover-card";
 
 export default function LangToggler() {
-  const [lang, setLang] = useState('en')
+	const [lang, setLang] = useState("en");
 
-  return (
-    <HoverCard>
-      <div>
-        <HoverCardTrigger asChild>
-          <Button
-            variant="outline"
-            size="icon"
-            onClick={() => setLang(lang === 'en' ? 'pt' : 'en')}
-          >
-            <LanguagesIcon className="size-5" />
-          </Button>
-        </HoverCardTrigger>
-        <HoverCardContent className='w-fit bg-zinc-700/50'>
-          <p className='text-xs text-zinc-400'>em breve...</p>
-        </HoverCardContent>
-      </div>
-    </HoverCard>
-  )
+	return (
+		<HoverCard>
+			<div>
+				<HoverCardTrigger asChild>
+					<Button
+						variant="outline"
+						size="icon"
+						onClick={() => setLang(lang === "en" ? "pt" : "en")}
+					>
+						<LanguagesIcon className="size-5" />
+					</Button>
+				</HoverCardTrigger>
+				<HoverCardContent className="w-fit dark:bg-zinc-700/50 bg-zinc-100/50">
+					<p className="text-xs text-zinc-400">em breve...</p>
+				</HoverCardContent>
+			</div>
+		</HoverCard>
+	);
 }

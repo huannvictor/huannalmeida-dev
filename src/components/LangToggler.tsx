523ff57cf669fbc3f72561fd38cb9e800c518588
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2026-03-09 17:36:36 -0300

    feat(template): adicionar componente Template com animações e controle de rolagem

--- src/components/LangToggler.tsx
+++ src/components/LangToggler.tsx
@@ -1,32 +1,52 @@
 "use client";
 
-import { LanguagesIcon } from "lucide-react";
-import { useState } from "react";
+import { usePathname, useRouter } from "@/src/i18n/navigation";
+import { Locale } from "@/src/i18n/routing";
+import { useLocale } from "next-intl";
+import { useParams } from "next/navigation";
 
-import { Button } from "../../components/ui/button";
+import { LanguagesIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import {
 	HoverCard,
 	HoverCardContent,
 	HoverCardTrigger,
-} from "../../components/ui/hover-card";
+} from "@/components/ui/hover-card";
 
 export default function LangToggler() {
-	const [lang, setLang] = useState("en");
+	const router = useRouter();
+	const locale = useLocale();
+	const pathname = usePathname();
+	const params = useParams();
+
+	const nextLocale = locale === "pt" ? "en" : "pt";
+
+	function changeLocale() {
+		router.replace(
+			// @ts-expect-error -- TypeScript will validate that only known `params`
+			// are used in combination with a given `pathname`.
+			{ pathname, params },
+			{ locale: nextLocale as Locale }
+		);
+	}
 
 	return (
-		<HoverCard>
+		<HoverCard openDelay={0} closeDelay={0}>
 			<div>
 				<HoverCardTrigger asChild>
 					<Button
 						variant="outline"
 						size="icon"
-						onClick={() => setLang(lang === "en" ? "pt" : "en")}
+						onClick={changeLocale}
+						className="relative"
 					>
 						<LanguagesIcon className="size-5" />
 					</Button>
 				</HoverCardTrigger>
-				<HoverCardContent className="w-fit dark:bg-zinc-700/50 bg-zinc-100/50">
-					<p className="text-xs text-zinc-400">em breve...</p>
+				<HoverCardContent className="w-fit dark:bg-zinc-800 bg-white border-border">
+					<p className="text-xs font-medium text-zinc-400">
+						{locale === "pt" ? "Switch to English" : "Mudar para Português"}
+					</p>
 				</HoverCardContent>
 			</div>
 		</HoverCard>
